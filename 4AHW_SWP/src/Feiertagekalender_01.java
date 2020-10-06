cbd774b3ce2b57011b8d34ccdc1a10d849d12386
SomSomDani <50622778+SomSomDani@users.noreply.github.com>
2020-10-06 20:29:26 +0200

    changes on methodes and charakters
    importing Javafx

--- 4AHW_SWP/src/Feiertagekalender_01.java
+++ 4AHW_SWP/src/Feiertagekalender_01.java
@@ -2,39 +2,59 @@ import java.io.*;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-public class Feiertagekalender_01 {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+public class Feiertagekalender_01 extends Application {
 	
 	
 	public static void main(String[] args) throws IOException
 	{
-		BufferedReader stdin =
-			      new BufferedReader ( new InputStreamReader ( System.in ) );
-				// Berechnung der Ostersonntage
-			    System.out.print("Bitte das Startjahr eingeben: ");
-			    //System.out.print("Bitte Anzahl der Jahre eingeben, wofür Feiertage berechnet werden sollten: ");
-			    String inData = stdin.readLine();
-			    int jahr = Integer.parseInt ( inData );
+				launch(args);
+				Scanner scanner = new Scanner(System.in);
+			    System.out.println("Bitte das Startjahr eingeben: ");
+			    int startyear;
+			    startyear = scanner.nextInt();
+			    System.out.println("Bitte das Endjahr eingeben: ");
+			    int endyear;
+			    endyear = scanner.nextInt();
 			    
-			    int montag=0;
-				int dienstag=0;
-				int mittwoch=0;
-				int donnerstag= 0;
-				int freitag=0;
-				int samstag = 0;
-				int sonntag=0;
+			    int monday=0;
+				int tuesday=0;
+				int wednesday=0;
+				int thursday= 0;
+				int friday=0;
+				int saturday = 0;
+				int sunday=0;
 				
-			    int a = jahr % 19;
-			    int b = jahr % 4;
-			    int c = jahr % 7;
+				List<LocalDate> mondays = new ArrayList<>();
+				List<LocalDate> tuesdays = new ArrayList<>();
+				List<LocalDate> wednesdays = new ArrayList<>();
+				List<LocalDate> thursdays = new ArrayList<>();
+				List<LocalDate> fridays = new ArrayList<>();
+				List<LocalDate> saturdays = new ArrayList<>();
+				List<LocalDate> sundays = new ArrayList<>();
+				
+				
 			    int monatOstern = 0;
 			    int monatCH = 0;
 			    int monatPfingstenSonntag = 0;
 			    int monatPfingstenMontag = 0;
 			    int monatFronleichnam = 0;
 			    
-			    int m = (8 * (jahr / 100) + 13) / 25 - 2;
-			    int s = jahr / 100 - jahr / 400 - 2;
+			    int a = startyear % 19;
+			    int b = startyear % 4;
+			    int c = startyear % 7;
+			    
+			    int m = (8 * (startyear / 100) + 13) / 25 - 2;
+			    int s = startyear / 100 - startyear / 400 - 2;
 			    m = (15 + s - m) % 30;
 			    int n = (6 + s) % 7;
 			    
@@ -54,6 +74,7 @@ public class Feiertagekalender_01 {
 			    int tagPfMo = 21 + d + e + 52;
 			    int tagFl = 21 + d + e + 62;
 			    
+			    // Berechnung der Ostersonntage
 			    if (tagOs > 31)
 			    {
 			      tagOs = tagOs % 31;
@@ -115,78 +136,99 @@ public class Feiertagekalender_01 {
 			    {
 			    	monatFronleichnam = 6;
 			    }
-			    /*	System.out.print("\nIm Jahr " + jahr + " ist der Ostersonntag am " + tagOs);
-			    	System.out.println("." + monatOstern + ".");
-			    	System.out.print("\nIm Jahr "+ jahr + " ist der Ostermontag am " + tagOm);
-			    	System.out.println("." + monatOstern + ".");
-			    	System.out.print("\nIm Jahr "+ jahr + " ist Christi Himmelfahrt am " + tagCH);
-			    	System.out.println("." + monatCH + ".");
-			    	System.out.print("\nIm Jahr "+ jahr + " ist Pfingstsonntag am " + tagPfSo);
-			    	System.out.println("." + monatPfingstenSonntag + ".");
-			    	System.out.print("\nIm Jahr "+ jahr + " ist Pfingstmontag am " + tagPfMo);
-			    	System.out.println("." + monatPfingstenMontag + ".");
-			    	System.out.print("\nIm Jahr "+ jahr + " ist Fromleichnam am " + tagFl);
-			    	System.out.println("." + monatFronleichnam + ".");*/
-			   
-			    ArrayList<LocalDate> freitage = new ArrayList<>();
-			    do
-			    {
-			    	try(Scanner scanner = new Scanner(new File("C:\\Users\\Platzhalter\\Desktop\\Backup Externe Festplatte\\Daniel SWP\\4AHW_Algorithm\\src\\FreiTage.csv")))
-			    	{
-			    		while(scanner.hasNextLine())
-			    		{
-			    			freitage.add(getRecordFromLine(scanner.nextLine(),jahr));
-			    		}
-			    	}
-			    	catch(FileNotFoundException e1)
-			    	{
-			    		e1.printStackTrace();
-			    	}
-			    	jahr ++;
-			    }
-			    while(jahr < jahr + 10);
 			    
-			    // Switch für die Tage hochzählen
-			    for(int i = 0; i<freitage.size();i++)
-				{
-			    	LocalDate date = freitage.get(i);
-			    	DayOfWeek dayS = date.getDayOfWeek();
-					switch(dayS)
-					{
-					case MONDAY: montag= (montag + 1) + (jahr * 2);
-					case TUESDAY: dienstag++;
-					case WEDNESDAY: mittwoch++;
-					case THURSDAY: donnerstag= (donnerstag + 1) + (jahr * 2);
-					case FRIDAY: freitag++;
-					case SATURDAY: samstag++;
-					case SUNDAY: sonntag= (sonntag +1) + (jahr * 2);
-					}
-				}
-			    System.out.println(montag);
-			    System.out.println(dienstag);
-			    System.out.println(mittwoch);
-			    System.out.println(donnerstag);
-			    System.out.println(freitag);
-			  }
-	
-	
-		//LocalDateTime local = LocalDateTime.parse("2020-01-01T12:39:10");
-		//DayOfWeek dayofweek = local.getDayOfWeek();
-		//System.out.println("Day of Week: " + dayofweek);
-	public static LocalDate getRecordFromLine(String line, int year)
-	{
-		LocalDate values = LocalDate.MIN;
-		int month, day;
-		try(Scanner rowScanner = new Scanner(line))
-		{
-			rowScanner.useDelimiter("#");
-			while(rowScanner.hasNextInt())
-			{
-				month=rowScanner.nextInt();
-				day = rowScanner.nextInt();
-				values = LocalDate.of(year,month,day);
-			}
-		}
-		return values;
+			    ArrayList<LocalDate> holidays = new ArrayList<>();
+			    
+			    holidaysGenerate(holidays, startyear, endyear);
+
+		        for (int i = 0; i < holidays.size(); i++) {
+
+		            if (holidays.get(i).getDayOfWeek().equals(DayOfWeek.MONDAY)) {
+		                mondays.add(holidays.get(i));
+		                monday++;
+		            }
+		            if (holidays.get(i).getDayOfWeek().equals(DayOfWeek.TUESDAY)) {
+		            	tuesdays.add(holidays.get(i));
+		                tuesday++;
+		            }
+		            if (holidays.get(i).getDayOfWeek().equals(DayOfWeek.WEDNESDAY)) {
+		            	wednesdays.add(holidays.get(i));
+		            	wednesday++;
+		            }
+		            if (holidays.get(i).getDayOfWeek().equals(DayOfWeek.THURSDAY)) {
+		            	thursdays.add(holidays.get(i));
+		                thursday++;
+		            }
+		            if (holidays.get(i).getDayOfWeek().equals(DayOfWeek.FRIDAY)) {
+		            	fridays.add(holidays.get(i));
+		                friday++;
+		            }
+		            if (holidays.get(i).getDayOfWeek().equals(DayOfWeek.SATURDAY)) {
+		            	saturdays.add(holidays.get(i));
+		                saturday++;
+		            }
+		            if (holidays.get(i).getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+		            	sundays.add(holidays.get(i));
+		                sunday++;
+		            }
+		        }
+		        
+		        holidaysOutput(monday, tuesday, wednesday, thursday, friday, saturday, sunday, mondays, tuesdays,
+		        				wednesdays, thursdays, fridays, saturdays, sundays);
+
 	}
-}
+	
+	public static void holidaysGenerate(List<LocalDate> holidays, int startyear, int endyear) {
+        for (int i = startyear; i <= startyear + endyear - startyear; i++) {
+        	holidays.add(LocalDate.of(i, 1, 1));
+        	holidays.add(LocalDate.of(i, 1, 6));
+        	holidays.add(LocalDate.of(i, 5, 1));
+        	holidays.add(LocalDate.of(i, 8, 15));
+        	holidays.add(LocalDate.of(i, 10, 26));
+        	holidays.add(LocalDate.of(i, 11, 1));
+        	holidays.add(LocalDate.of(i, 12, 8));
+        	holidays.add(LocalDate.of(i, 12, 25));
+        	holidays.add(LocalDate.of(i, 12, 26));
+
+        	
+        }
+    }
+	 public static void holidaysOutput(int mo, int di, int mi, int don, int fr, int sa, int so, List<LocalDate> Montage,
+             List<LocalDate> Dienstage, List<LocalDate> Mittwoche, List<LocalDate> Donnerstage,
+             List<LocalDate> Freitage, List<LocalDate> Samstage, List<LocalDate> Sonntage) {
+				System.out.println("Montage: " + mo + " " + Montage);
+				System.out.println("Dienstage: " + di + " " + Dienstage);
+				System.out.println("Mittwoche: " + mi + " " + Mittwoche);
+				System.out.println("Donnerstage: " + don + " " + Donnerstage);
+				System.out.println("Freitage: " + fr + " " + Freitage);
+				System.out.println("Samstage: " + sa + " " + Samstage);
+				System.out.println("Sonntage: " + so + " " + Sonntage);
+	 }
+	 @SuppressWarnings({ "unchecked", "rawtypes" })
+	public void start(Stage primaryStage)
+	 {
+		 try
+		 {
+			 final CategoryAxis xAxis = new CategoryAxis();
+			 final NumberAxis yAxis = new NumberAxis();
+			 final BarChart<String, Number> barChart = new BarChart<String,Number>(xAxis,yAxis);
+			 
+			 barChart.setTitle("Feiertage in den Jahren 2020 bis 2030 ");
+			 xAxis.setLabel("Wochentage");
+			 yAxis.setLabel("Anzahl der Feiertage");
+			 
+			 	XYChart.Series series1 = new XYChart.Series();
+		        series1.setName("2020-2030");
+		        series1.getData().add(new XYChart.Data("Montage", holidaysOutput(montage)));
+
+	
+			    Scene scene = new Scene(barChart, 640, 480);
+			    barChart.getData().addAll(series1);
+
+				primaryStage.setScene(scene);
+				primaryStage.show();
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		 }
+	 }
