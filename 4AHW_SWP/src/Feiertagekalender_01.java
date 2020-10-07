c2d8001ce0804195483744d46875ba77a570a38f
SomSomDani <50622778+SomSomDani@users.noreply.github.com>
2020-10-07 12:38:20 +0200

    Adding JavaFX Methode
    säulendiagramm mit Days

--- 4AHW_SWP/src/Feiertagekalender_01.java
+++ 4AHW_SWP/src/Feiertagekalender_01.java
@@ -4,21 +4,34 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-/*import javafx.application.Application;
+import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.chart.Axis;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.stage.Stage; */
+import javafx.stage.Stage; 
 
-public class Feiertagekalender_01// extends Application 
+public class Feiertagekalender_01 extends Application 
 {
+	static int monday=0;
+	static int tuesday=0;
+	static int wednesday=0;
+	static int thursday= 0;
+	static int friday=0;
+	static int saturday = 0;
+	static int sunday=0;
 	
-	
+	static List<LocalDate> mondays = new ArrayList<>();
+	static List<LocalDate> tuesdays = new ArrayList<>();
+	static List<LocalDate> wednesdays = new ArrayList<>();
+	static List<LocalDate> thursdays = new ArrayList<>();
+	static List<LocalDate> fridays = new ArrayList<>();
+	static List<LocalDate> saturdays = new ArrayList<>();
+	static List<LocalDate> sundays = new ArrayList<>();
 	public static void main(String[] args) throws IOException
 	{
-				// launch(args);
 				Scanner scanner = new Scanner(System.in);
 			    System.out.println("Bitte das Startjahr eingeben: ");
 			    int startyear;
@@ -26,25 +39,12 @@ public class Feiertagekalender_01// extends Application
 			    System.out.println("Bitte das Endjahr eingeben: ");
 			    int endyear;
 			    endyear = scanner.nextInt();
+			 
+				// Listen Feiertagen
+				List<LocalDate> dynamicHolidays = new ArrayList<>();   
+			    ArrayList<LocalDate> holidays = new ArrayList<>();
 			    
-			    int monday=0;
-				int tuesday=0;
-				int wednesday=0;
-				int thursday= 0;
-				int friday=0;
-				int saturday = 0;
-				int sunday=0;
-				
-				List<LocalDate> mondays = new ArrayList<>();
-				List<LocalDate> tuesdays = new ArrayList<>();
-				List<LocalDate> wednesdays = new ArrayList<>();
-				List<LocalDate> thursdays = new ArrayList<>();
-				List<LocalDate> fridays = new ArrayList<>();
-				List<LocalDate> saturdays = new ArrayList<>();
-				List<LocalDate> sundays = new ArrayList<>();
-				
-				List<LocalDate> dynamicHolidays = new ArrayList<>();
-				
+			    // dynamische Feiertage berechnen
 			    for (int i = startyear; i <= startyear + endyear - startyear; i++)
 			    {
 			    	int a = startyear % 19;
@@ -83,28 +83,35 @@ public class Feiertagekalender_01// extends Application
 				    {
 				      tagOs = tagOs % 31;
 				      monatOstern = 5;
+				      dynamicHolidays.add(LocalDate.of(i,monatOstern,tagOs));
 				    }
 				    if (tagOs <= 31)
-				    {  monatOstern = 4; }
+				    {  monatOstern = 4;
+				    	dynamicHolidays.add(LocalDate.of(i,monatOstern,tagOs));
+				    }
 				    // Berechnung der Ostermontage
 				    if (tagOm >31)
 				    {
 				    	tagOm = tagOm %31;
 				    	monatOstern=5;
+				    	dynamicHolidays.add(LocalDate.of(i,monatOstern,tagOm));
 				    }
 				    if (tagOm <= 31)
 				    {
 				    	monatOstern=4;
+				    	dynamicHolidays.add(LocalDate.of(i,monatOstern,tagOm));
 				    }
 				    // Berechnung der Christi Himmelfahrten
 				    if (tagCH >31)
 				    {
 				    	tagCH = tagCH %31;
 				    	monatCH=6;
+				    	dynamicHolidays.add(LocalDate.of(i,monatCH,tagCH));
 				    }
 				    if (tagCH <= 31)
 				    {
 				    	monatCH=5;
+				    	dynamicHolidays.add(LocalDate.of(i,monatCH,tagCH));
 				    }
 				    // Berechnung von Pfingstsonntag
 				    if (tagPfSo > 31)
@@ -115,43 +122,42 @@ public class Feiertagekalender_01// extends Application
 				    		tagPfSo += 31;
 				    	}
 				    	monatPfingstenSonntag=6;
+				    	dynamicHolidays.add(LocalDate.of(i,monatPfingstenSonntag,tagPfSo));
 				    }
 				    if (tagPfSo <= 31)
 				    {
 				    	monatPfingstenSonntag=5;
+				    	dynamicHolidays.add(LocalDate.of(i,monatPfingstenSonntag,tagPfSo));
 				    }
 				    // Berechnung von Pfingstmontag
 				    if (tagPfMo > 31)
 				    {
 				    	tagPfMo = tagPfMo % 31;
 				    	monatPfingstenMontag=7;
+				    	dynamicHolidays.add(LocalDate.of(i,monatPfingstenMontag,tagPfMo));
 				    }
 				    if(tagPfMo <= 31)
 				    {
 				    	monatPfingstenMontag=6;
+				    	dynamicHolidays.add(LocalDate.of(i,monatPfingstenMontag,tagPfMo));
 				    }
 				    // Berechnung von Fromleichnam
 				    if(tagFl > 31)
 				    {
 				    	tagFl = tagFl % 31;
 				    	monatFronleichnam = 7;
+				    	dynamicHolidays.add(LocalDate.of(i,monatFronleichnam,tagFl));
 				    }
 				    if(tagFl <= 31)
 				    {
 				    	monatFronleichnam = 6;
+				    	dynamicHolidays.add(LocalDate.of(i,monatFronleichnam,tagFl));
 				    }
-				    dynamicHolidays.add(LocalDate.of(i,monatOstern,tagOs));
-				    dynamicHolidays.add(LocalDate.of(i,monatOstern,tagOm));
-				    dynamicHolidays.add(LocalDate.of(i,monatCH,tagCH));
-				    dynamicHolidays.add(LocalDate.of(i,monatPfingstenSonntag,tagPfSo));
-				    dynamicHolidays.add(LocalDate.of(i,monatPfingstenMontag,tagPfMo));
-				    dynamicHolidays.add(LocalDate.of(i,monatFronleichnam,tagFl));
 			    }
 			    
-			    ArrayList<LocalDate> holidays = new ArrayList<>();
-			    
+			    // Feiertage generieren
 			    holidaysGenerate(holidays, startyear, endyear);
-
+			    
 			    // fixen Feiertage
 		        for (int i = 0; i < holidays.size(); i++) {
 
@@ -184,45 +190,26 @@ public class Feiertagekalender_01// extends Application
 		                sunday++;
 		            }
 		        }
-		        // dynamische Feiertage
-		        for (int i = 0; i < dynamicHolidays.size(); i++) {
+			    for (int i = 0; i < dynamicHolidays.size(); i++) {
 
 		            if (dynamicHolidays.get(i).getDayOfWeek().equals(DayOfWeek.MONDAY)) {
 		                mondays.add(dynamicHolidays.get(i));
 		                monday++;
 		            }
-		            if (dynamicHolidays.get(i).getDayOfWeek().equals(DayOfWeek.TUESDAY)) {
-		            	tuesdays.add(dynamicHolidays.get(i));
-		                tuesday++;
-		            }
-		            if (dynamicHolidays.get(i).getDayOfWeek().equals(DayOfWeek.WEDNESDAY)) {
-		            	wednesdays.add(dynamicHolidays.get(i));
-		            	wednesday++;
-		            }
 		            if (dynamicHolidays.get(i).getDayOfWeek().equals(DayOfWeek.THURSDAY)) {
 		            	thursdays.add(dynamicHolidays.get(i));
 		                thursday++;
-		            }
-		            if (dynamicHolidays.get(i).getDayOfWeek().equals(DayOfWeek.FRIDAY)) {
-		            	fridays.add(dynamicHolidays.get(i));
-		                friday++;
-		            }
-		            if (dynamicHolidays.get(i).getDayOfWeek().equals(DayOfWeek.SATURDAY)) {
-		            	saturdays.add(dynamicHolidays.get(i));
-		                saturday++;
 		            }
 		            if (dynamicHolidays.get(i).getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
 		            	sundays.add(dynamicHolidays.get(i));
 		                sunday++;
 		            }
 		        }
-		        
-		        
+			    // Feiertage ausgeben
 		        holidaysOutput(monday, tuesday, wednesday, thursday, friday, saturday, sunday, mondays, tuesdays,
 		        				wednesdays, thursdays, fridays, saturdays, sundays);
-
+		        launch(args);
 	}
-	
 	public static void holidaysGenerate(List<LocalDate> holidays, int startyear, int endyear) {
         for (int i = startyear; i <= startyear + endyear - startyear; i++) {
         	holidays.add(LocalDate.of(i, 1, 1));
@@ -238,6 +225,7 @@ public class Feiertagekalender_01// extends Application
         	
         }
     }
+	
 	 public static void holidaysOutput(int mo, int di, int mi, int don, int fr, int sa, int so, List<LocalDate> Montage,
              List<LocalDate> Dienstage, List<LocalDate> Mittwoche, List<LocalDate> Donnerstage,
              List<LocalDate> Freitage, List<LocalDate> Samstage, List<LocalDate> Sonntage) {
@@ -249,31 +237,36 @@ public class Feiertagekalender_01// extends Application
 				System.out.println("Samstage: " + sa + " " + Samstage);
 				System.out.println("Sonntage: " + so + " " + Sonntage);
 	 }
-	/* @SuppressWarnings({ "unchecked", "rawtypes" })
-	public void start(Stage primaryStage)
+	 
+	 @SuppressWarnings({ "unchecked", "rawtypes" })
+	 public void start(Stage primaryStage) throws Exception
 	 {
 		 try
 		 {
 			 final CategoryAxis xAxis = new CategoryAxis();
 			 final NumberAxis yAxis = new NumberAxis();
-			 final BarChart<String, Number> barChart = new BarChart<String,Number>(xAxis,yAxis);
+			 final BarChart<String,Number > barChart = new BarChart<String,Number>(xAxis,yAxis);
 			 
-			 barChart.setTitle("Feiertage in den Jahren 2020 bis 2030 ");
+			 barChart.setTitle("Feiertage in den Jahren 2021 bis 2031 ");
 			 xAxis.setLabel("Wochentage");
 			 yAxis.setLabel("Anzahl der Feiertage");
 			 
-			 	//XYChart.Series series1 = new XYChart.Series();
-		        //series1.setName("2020-2030");
-		        //series1.getData().add(new XYChart.Data("Montage", holidaysOutput(montage)));
+			 	XYChart.Series series1 = new XYChart.Series();
+		        series1.setName("2021-2031");
+		        series1.getData().add(new XYChart.Data("Montage", monday));
+		        series1.getData().add(new XYChart.Data("Tuesdays", tuesday));
+		        series1.getData().add(new XYChart.Data("Wednesdays", wednesday));
+		        series1.getData().add(new XYChart.Data("Thursdays", thursday));
+		        series1.getData().add(new XYChart.Data("Fridays", friday));
+		        series1.getData().add(new XYChart.Data("Saturdays", saturday));
+		        series1.getData().add(new XYChart.Data("Sundays", sunday));
 
-	
-			   // Scene scene = new Scene(barChart, 640, 480);
-			   // barChart.getData().addAll(series1);
-
-				//primaryStage.setScene(scene);
+		        barChart.getData().addAll(series1);
+		        Scene scene = new Scene(barChart, 640, 480);
+				primaryStage.setScene(scene);
 				primaryStage.show();
 			} catch(Exception e) {
 				e.printStackTrace();
 			}
-		 }*/
-	 }
+		 }
+}
